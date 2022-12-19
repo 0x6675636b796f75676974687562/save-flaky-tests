8da9dfcf07db7ea3f313de895e432202a9885da4
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2022-12-19 20:22:36 +0300

    Add the project skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+    id("org.cqfn.diktat.diktat-gradle-plugin") version "1.2.3"
+    eclipse
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven {
+        name = "0x6675636b796f75676974687562/save-flaky-tests"
+        url = uri("https://maven.pkg.github.com/0x6675636b796f75676974687562/save-flaky-tests")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+internal val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+
+internal val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/0x6675636b796f75676974687562/save-flaky-tests")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            groupId = "com.example"
+            version = "0.0.1-SNAPSHOT"
+
+            from(components["java"])
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+rootProject.name = "save-flaky-tests"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.9"
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
